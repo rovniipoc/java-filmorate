0a492126972e1389ee279a1cfeaafc0418949f57
Estet <7vt-blest@rambler.ru>
2024-09-08 23:17:00 +0400

    Реализовать валидация моделей через аннотации @NotNull и @NotBlank

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,7 +11,12 @@ import java.time.LocalDate;
 @Data
 public class User {
     Long id;
+    @Email
+    @NotNull
+    @NotBlank
     String email;
+    @NotNull
+    @NotBlank
     String login;
     String name;
     LocalDate birthday;
